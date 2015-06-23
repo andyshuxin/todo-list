18666bcd02c9c8770c1211246985be002c3d92cf
Andy Shu Xin <andyshuxin@gmail.com>
2015-06-23 13:17:41 +0800

    lint fixes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*jshint browser:true */
-'use strict';
+/*global confirm */
 
 var DEBUG = false;
 
@@ -10,28 +10,31 @@ var task_list = document.getElementById('task_list');
 var id_counter = 0;
 
 if (DEBUG) {
-    for (var i=0; i<10; i++) {
-        var li = document.createElement("li");
-        var textNode = document.createTextNode(i);
+    var i, li, textNode;
+    for (i = 0; i < 10; i += 1) {
+        li = document.createElement("li");
+        textNode = document.createTextNode(i);
         li.appendChild(textNode);
         li.setAttribute('id', id_counter.toString());
-        id_counter++;
+        id_counter += 1;
         task_list.appendChild(li);
     }
 }
 
 btn.onclick = function () {
+    'use strict';
     var li = document.createElement("li");
-    textNode = document.createTextNode(input.value);
+    var textNode = document.createTextNode(input.value);
     li.appendChild(textNode);
     li.setAttribute('id', id_counter.toString());
-    id_counter++;
+    id_counter += 1;
     task_list.appendChild(li);
 };
 
-task_list.addEventListener('click', function(e) {
+task_list.addEventListener('click', function (e) {
+    'use strict';
     var li = e.target;
-    if (li.tagName.toUpperCase() === 'LI'){
+    if (li.tagName.toUpperCase() === 'LI') {
         if (confirm('Seriously?')) {
             task_list.removeChild(li);
         }
